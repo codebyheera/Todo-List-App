794dbb8455c159334f334ce51ad6884e56b1db81
Muhammad Subhan Naeem <muhammadsubhan189345@gmail.com>
2025-09-10 02:27:34 +0500

    Todo-List App

---
+++ script.js
@@ -0,0 +1,34 @@
+let input = document.getElementById('in-box');
+let container = document.getElementById('ls-container');
+function addtask(){
+    if(input.value === ''){
+        alert('You must write something!')
+    }
+    else{
+       let li =  document.createElement('li');
+        li.innerHTML = input.value;
+        container.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    input.value = '';
+    saveData();
+}
+container.addEventListener('click', function(e){
+if(e.target.tagName === 'LI'){
+    e.target.classList.toggle('checked');
+    saveData();
+}
+else if (e.target.tagName === 'SPAN'){
+    e.target.parentElement.remove();
+    saveData();
+}
+}, false);
+function saveData(){
+    localStorage.setItem('data',container.innerHTML)
+}
+function showData(){
+container.innerHTML = localStorage.getItem('data');
+}
+showData();
